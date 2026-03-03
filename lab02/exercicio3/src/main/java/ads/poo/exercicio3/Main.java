19d57680618492381b597191aae1d09c45bab4ca
kaleu-b <kaleu.b@aluno.ifsc.edu.br>
2026-03-03 20:23:41 -0300

    Adicionando arquivos referentes ao exercicio3.
    Adicionando arquivos iniciais referente ao exercicio3, com um esboço inicial do código.

---
+++ lab02/exercicio3/src/main/java/ads/poo/exercicio3/Main.java
@@ -0,0 +1,35 @@
+package ads.poo.exercicio3;
+
+public class Main {
+    static void main(String[] args) {
+        int altura;
+        int largura;
+
+
+            if (args.length == 0) {
+                IO.println("Argumentos vazios. Sayonara");
+            } else if (args[0].equals("losango")) {
+                altura = Integer.parseInt(args[1]);
+                losango(altura);
+            } else if (args[0].equals("triangulo")) {
+                altura = Integer.parseInt(args[1]);
+                triangulo(altura);
+            } else if (args[0].equals("retangulo")) {
+                altura = Integer.parseInt(args[1]);
+                largura = Integer.parseInt(args[1]);
+                retangulo(altura, largura);
+            }
+        }
+    }
+    public static void triangulo(int altura) {
+        IO.println("isso será um triangulo maneiro no futuro :)");
+    }
+
+    public static void losango(int altura) {
+        IO.println("Isto será um losango mt maneirinho no futuro :)");
+    }
+
+    public static void retangulo(int altura, int largura) {
+        IO.println("Isto será um retangulo mt maneirinho no futuro :)");
+    }
+}
