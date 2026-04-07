d0dc4e1223c320d6f71562616307a744a5dd1c48
kaleu-b <kaleu.b@aluno.ifsc.edu.br>
2026-04-07 20:22:23 -0300

    refinando a classe conta da aula 12

--- aula12/untitled/src/main/java/ads/poo/aula12/App.java
+++ aula12/untitled/src/main/java/ads/poo/aula12/App.java
@@ -2,11 +2,11 @@ package ads.poo.aula12;
 
 import java.util.ArrayList;
 
-public
-class App {
+public class App {
     ArrayList<Conta> contas;
-    public
-    App() { contas = new ArrayList<>(); }
+    public App() {
+        contas = new ArrayList<>();
+    }
 
     void menu(int opcao) {
         switch (opcao) {
@@ -14,7 +14,7 @@ class App {
             case 2 -> listar();
             case 3 -> depositar();
             case 4 -> sacar();
-            case 5 -> IO.println("ADSDFAS");
+            case 5 -> IO.println("Saindo...");
         }
 
     }
@@ -29,33 +29,53 @@ class App {
     }
 
     void listar () {
-        IO.println(contas.toString());
+        contas.forEach(IO::println);
     }
 
     void depositar (){
-        var i = Integer.parseInt(IO.readln("digite o indice da conta em que deseja depositar: "));
         var v = Integer.parseInt(IO.readln("digite o valor do deposito: "));
-        contas.get(i).depositar(v);
+        var procura = IO.readln("Digite o número da conta ou o nome da pessoa: ");
+
+        for (Conta c : contas) {
+            if (c.isValido(procura)) {
+                c.depositar(v);
+                return;
+            }
+        }
+        IO.println("Conta não encontrada.");
     }
 
-    void sacar(){
-        var i = Integer.parseInt(IO.readln("digite o indice da conta em que deseja depositar: "));
-        var v = Integer.parseInt(IO.readln("digite o valor do deposito: "));
-        contas.get(i).sacar(v);
+    void sacar() {
+        var v = Integer.parseInt(IO.readln("digite o valor do saque: "));
+        var procura = IO.readln("Digite o número da conta ou o nome da pessoa:");
+
+        for (Conta c : contas) {
+            var saldoAnterior = c.getSaldo();
+            if (c.isValido(procura)) {
+                c.sacar(v);
+                if (c.getSaldo() == saldoAnterior){
+                    IO.println("Não foi possível realizar o depósito.");
+                }else {
+                    IO.println(String.format("Depósito realiado. %.2f -> %.2f", saldoAnterior, c.getSaldo()));
+                }
+                return;
+            }
+        }
+        IO.println("Conta não encontrada.");
     }
 
-    static void main(String[]args){
+    static void main(String[] args) {
         App app = new App();
         int opcao;
         do {
-            IO.println( "..:: Menu ::..\n" +
-                    "1 - cadastrar conta\n"+
-                    "2 - listar todas as contas\n"+
-                    "3 - depositar em uma conta\n"+
-                    "4 - sacar em uma conta\n"+
-                    "5 - sair");
+            IO.println("..:: Menu ::..\n"
+                    + "1 - cadastrar conta\n"
+                    + "2 - listar todas as contas\n"
+                    + "3 - depositar em uma conta\n"
+                    + "4 - sacar em uma conta\n"
+                    + "5 - sair");
             opcao = Integer.parseInt(IO.readln("Entre com uma opcao: "));
             app.menu(opcao);
-        }while (opcao != 5);
+        } while (opcao != 5);
     }
 }
--- aula12/untitled/src/main/java/ads/poo/aula12/Conta.java
+++ aula12/untitled/src/main/java/ads/poo/aula12/Conta.java
@@ -1,37 +1,51 @@
 package ads.poo.aula12;
 
-public
-class Conta {
-    private
-    final String NOME;
-    private
-    final String NUM_CON;
-    private
-    double saldo;
+public class Conta {
+    private final String NOME;
+    private final String NUM_CON;
+    private double saldo;
 
-    public
-    Conta(String NOME, String NUM_CON, double saldo) {
+    public Conta(String NOME, String NUM_CON, double saldo) {
         this.NOME = NOME;
         this.NUM_CON = NUM_CON;
         this.saldo = (saldo < 0) ? 0 : saldo;
     }
 
-    public
-    String getNOME() { return NOME; }
+    public String getNOME() {
+        return NOME;
+    }
 
-    public
-    String getNUM_CON() { return NUM_CON; }
+    public boolean isNome(String n) {
+        return n.equals(this.NOME);
+    }
 
-    public
-    double getSaldo() { return saldo; }
+    public boolean isConta(String con) {
+        return con.equals(this.NUM_CON);
+    }
 
-    public
-    void depositar(double valor) { this.saldo += (valor < 0) ? 0 : valor; }
+    public boolean isValido(String v){
+     return isConta(v) || isNome(v);
+    }
 
-    public
-    void sacar(double valor) { this.saldo -= (valor > saldo) ? 0 : valor; }
+    public String getNUM_CON() {
+        return NUM_CON;
+    }
 
-    @Override public String toString() {
-        return String.format("Saldo: R$ %.2f\nNúmero da conta: %s\nNome do titular: %s", saldo, NUM_CON, NOME);
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void depositar(double valor) {
+        this.saldo += (valor < 0) ? 0 : valor;
+    }
+
+    public void sacar(double valor) {
+        this.saldo -= (valor > saldo) ? 0 : valor;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Saldo: R$ %.2f\nNúmero da conta: %s\nNome do titular: %s",
+                saldo, NUM_CON, NOME);
     }
 }
