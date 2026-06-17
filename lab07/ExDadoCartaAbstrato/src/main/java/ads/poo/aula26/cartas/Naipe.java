3f30393d3d03940220920f09c7428794f2fc4cc9
kaleu-b <kaleu.b@aluno.ifsc.edu.br>
2026-06-17 19:58:34 -0300

    Adicionando início das enumerações

--- lab07/ExDadoCartaAbstrato/src/main/java/ads/poo/aula26/cartas/Naipe.java
+++ lab07/ExDadoCartaAbstrato/src/main/java/ads/poo/aula26/cartas/Naipe.java
@@ -1,4 +1,22 @@
 package ads.poo.aula26.cartas;
 
 public enum Naipe {
+
+    PAUS("Paus", 'p'),
+    OURO("Ouro", 'o'),
+    COPAS("Copas", 'c'),
+    ESPADA("Espada", 'e');
+
+    private final String nome;
+    private final char inicial;
+
+    Naipe(String nome, char inicial) {
+        this.nome = nome;
+        this.inicial = inicial;
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(this.inicial);
+    }
 }
--- lab07/ExDadoCartaAbstrato/src/main/java/ads/poo/aula26/cartas/Valor.java
+++ lab07/ExDadoCartaAbstrato/src/main/java/ads/poo/aula26/cartas/Valor.java
@@ -1,4 +1,29 @@
 package ads.poo.aula26.cartas;
 
 public enum Valor {
+
+    ACE("1"),
+    DOIS("2"),
+    TRES("3"),
+    QUATRO("4"),
+    CINCO("5"),
+    SEIS("6"),
+    SETE("7"),
+    OITO("8"),
+    NOVE("9"),
+    DEZ("10"),
+    VALETE("j"),
+    DAMA("k"),
+    REI("q");
+
+    private final String valor;
+
+    Valor(String valor) {
+        this.valor = valor;
+    }
+
+    @Override
+    public String toString() {
+      return this.valor;
+    }
 }
